b6566b42bf9dc82efaa2dbfd83c2b7a5dc94e7d4
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-12-11 09:30:53 +0700

    Add Callback app models

--- callback/models.py
+++ callback/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя клиента")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_processed = models.BooleanField(default=False, verbose_name="Обработана")
+
+    class Meta:
+        verbose_name = "Заявка"
+        verbose_name_plural = "Заявки"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Заявка: {self.first_name} ({self.phone})"
+
